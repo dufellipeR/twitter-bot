e55d392a2306f11500436fbac17cb9b6733c008c
dufellipeR <eduardo.fellipe30@gmail.com>
2020-03-26 02:17:36 -0300

    Addprint

--- services.py
+++ services.py
@@ -9,6 +9,7 @@ def tweepy_auth(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET):
 
 def retweet_favorites(api, username):
     for tweet in api.favorites(username):
+        print(tweet.user.name, tweet.text)
         try:
             api.retweet(tweet.id)
         except tweepy.TweepError as e:
